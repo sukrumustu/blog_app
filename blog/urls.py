cf21124e0d39624a91e354f77b632ef95ced6f1d
smustu <smustu1981@gmail.com>
2023-07-18 10:33:13 +0300

    update-deleteapis

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import PostListApiView, PostCreateApi, PostDetailApi
+from .views import PostListApiView, PostCreateApi, PostDetailApi, PostUpdateApi, PostDeleteApi
 
 # router = routers.DefaultRouter()
 # router.register('posts',PostMVS)
@@ -11,6 +11,8 @@ urlpatterns = [
     path("create/", PostCreateApi.as_view()),
     # path("detail/<int:pk>/", PostDetailApi.as_view()),
     path("detail/<str:slug>/", PostDetailApi.as_view()),
+    path("update/<str:slug>/", PostUpdateApi.as_view()),
+    path("delete/<str:slug>/", PostDeleteApi.as_view()),
     # path("", include(router.urls)),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -58,14 +58,65 @@ class PostUpdateApi(RetrieveUpdateAPIView):
     
     lookup_field = 'slug'
     
-    def retrieve(self, request, *args, **kwargs):
+     
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
         instance = self.get_object()
         if instance.author == self.request.user:
-            serializer = self.get_serializer(instance)
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            
+            if getattr(instance, '_prefetched_objects_cache', None):
+                # If 'prefetch_related' has been applied to a queryset, we need to
+                # forcibly invalidate the prefetch cache on the instance.
+                instance._prefetched_objects_cache = {}
             return Response(serializer.data)
-        return Response({'message': 'There is no available post within your search parameters.'})
+        
+        return Response({'message': 'You are not authorized to update a post.'})
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+class PostDeleteApi(DestroyAPIView):
     
-    def update(self, request, *args, **kwargs):
+    queryset = Post.objects.all()
+    serializer_class=PostSerializer
+    permission_classes=[IsAuthenticated]
+    
+    lookup_field = 'slug'
+    
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author == self.request.user:
+        
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({'message': 'You are not authorized to delete a post.'})
+        
+
+    def perform_destroy(self, instance):
+        instance.delete()
+    
+    
+    
+    # def destroy(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+        
+    #     #!####### DELETE Product Stock ########
+    #     product = Product.objects.get(id=instance.product_id)
+    #     product.stock += instance.quantity
+    #     product.save()
+    #     #!##################################
+        
+    #     self.perform_destroy(instance)
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    
+    
+    
         
     
     
